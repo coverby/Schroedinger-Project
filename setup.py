286c3f102be516ea8c3276d139f4564c2ce29d77
Clyde Overby <coverby@ur.rochester.edu>
2017-12-17 10:13:44 -0500

    Adapting setup scripts from langevin project

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 with open('README.md') as f:
       long_description = ''.join(f.readlines())
 
-setup(name='Langevin477CO',
-      version='.1',
-      description='CHE 477 Langevin Simulator',
+setup(name='Schro1D',
+      version='.01',
+      description='CHE 477 1D Numerical Schroedinger Solver',
       long_description=long_description,
       author='Clyde Overby',
-      packages=['Langevin477CO'],
+      packages=['Schro1D'],
       entry_points=
       {
-            'console_scripts': ["langevin=Langevin477CO.lang:start"]
+            'console_scripts': ["schro=Schro1D.lang:start"]
       }
      )
